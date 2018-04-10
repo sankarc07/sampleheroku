8312795e7794227af8a3ccb40f09e6d5556e7314
Sankar\Sankar Chellamuthu <schellamuthu.consultant@consumer.org>
2018-04-10 22:43:19 +0530

    Add sample heroku project

---
+++ src/main/java/com/heroku/sample/herokutest/HerokutestApplication.java
@@ -0,0 +1,12 @@
+package com.heroku.sample.herokutest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokutestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokutestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/heroku/sample/herokutest/HerokutestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.heroku.sample.herokutest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HerokutestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
